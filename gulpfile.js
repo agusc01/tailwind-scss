dbce1ec481ef304e524e4b87efc5dd7163bee694
Agustin <33077305+agusc01@users.noreply.github.com>
2022-03-19 17:54:54 -0300

    No funciona correctamente al actualizar el scss tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-// Initialize modules
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const browsersync = require('browser-sync').create();
+const tailwind = require('tailwindcss');
+const tailwindConfig = require('./tailwind.config');
 
-// Sass Task
 function scssTask() {
   return src('src/scss/style.scss', { sourcemaps: true })
     .pipe(sass())
@@ -14,7 +14,15 @@ function scssTask() {
     .pipe(dest('public/dist', { sourcemaps: '.' }));
 }
 
-// Browsersync
+function tailwindcssTask() {
+  return src('src/tailwind/tailwind.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(
+      postcss([tailwind(tailwindConfig), autoprefixer(), cssnano()])
+    )
+    .pipe(dest('public/dist', { sourcemaps: '.' }));
+}
+
 function browserSyncServe(cb) {
   browsersync.init({
     server: {
@@ -34,9 +42,16 @@ function browserSyncReload(cb) {
   cb();
 }
 
-// Watch Task
 function watchTask() {
   watch('public/*.html', browserSyncReload);
+  watch(
+    `${tailwindConfig.content[0]}`,
+    series(tailwindcssTask, browserSyncReload)
+  );
+  watch(
+    `${tailwindConfig.content[1]}`,
+    series(tailwindcssTask, browserSyncReload)
+  );
   watch(
     ['src/scss/**/*.scss', 'src/js'],
     series(scssTask, browserSyncReload)
@@ -44,4 +59,9 @@ function watchTask() {
 }
 
 // Default Gulp Task
-exports.default = series(scssTask, browserSyncServe, watchTask);
+exports.default = series(
+  scssTask,
+  tailwindcssTask,
+  browserSyncServe,
+  watchTask
+);
